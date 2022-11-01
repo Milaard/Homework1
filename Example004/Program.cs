70a338a891b042d0770079e57d9586ee8ec32ef9
Milaard <116643003+Milaard@users.noreply.github.com>
2022-11-01 13:48:20 +0300

    Add Example004

---
+++ Example004/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выход показывает все четные числа от 1 до N
+Console.WriteLine("Введите число > 0");
+int num = int.Parse(Console.ReadLine());
+
+while(num>2)
+{
+    if(num % 2 == 0)
+    {
+        Console.Write( num + "," );
+    }    
+    num = num - 1;
+}
+if ( num == 2)
+{
+    Console.Write(num);
+}
+else
+{
+    Console.Write("Нет четных чисел");
+}
